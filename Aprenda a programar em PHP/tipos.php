1fd5e8a4c618a1d4e5a7f7858bbd7ec9f33fa002
eullersonsoares <eullersonsoares@gmail.com>
2023-07-10 19:55:58 -0300

    tipos de dados

---
+++ Aprenda a programar em PHP/tipos.php
@@ -0,0 +1,20 @@
+<?php 
+    // Inteiro (Integer):
+    $idade = 22;
+
+    //Real (Double ou Float):
+    $salario = 1000.31;
+
+    //pegando o tipo da variável: 
+    echo gettype($salario);
+
+    //textos
+    $texto = "Meu nome é Eullerson";
+
+    echo gettype($texto);
+
+    //boolean
+    $verdadeiro = true;
+    $falso = false;
+
+    echo gettype($verdadeiro);
