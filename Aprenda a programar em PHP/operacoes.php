7f43926ff5830858e6bdb56965b529163d33dd09
eullersonsoares <eullersonsoares@gmail.com>
2023-07-10 19:31:01 -0300

    operações com php

---
+++ Aprenda a programar em PHP/operacoes.php
@@ -0,0 +1,16 @@
+<?php
+
+    $idade = 22;
+    $idadeDaqui10Anos = $idade + 10;
+    $idadeHa5Anos = $idade - 5;
+    $dobroDaIdade = $idade * 2;
+    $metadeDaIdade = $idade/2;
+    $idadeAoCubo = $idade ** 2;
+    $restoDaDivisao = $idade%3;
+
+    echo "Daqui há 10 anos: " . $idadeDaqui10Anos 
+        ." mas, há 5 anos atrás era: " . $idadeHa5Anos 
+        ." O dobro é: " . $dobroDaIdade 
+        ." e metade é: " . $metadeDaIdade
+        ." idade a ocubo " . $idadeAoCubo
+        ." Resto da divisão por 7: " . $restoDaDivisao;
