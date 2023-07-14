06178a420adf207114f85c78f32c37b158897aaf
eullersonsoares <eullersonsoares@gmail.com>
2023-07-14 18:52:28 -0300

    aquivo de loop com continue e break

--- Aprenda a programar em PHP/repeticao-2.php
+++ Aprenda a programar em PHP/repeticao-2.php
@@ -2,5 +2,10 @@
 
     //Para incrementar um valor a $contador, podemos utilizar também o += 1
     for ($contador = 1; $contador <=15; $contador++) {
+        if($contador == 13) {
+            //continue; //para pular
+            break; //para parar a execução antes que a condição do loop seja atendida.
+        }
+
         echo "#$contador\n";
     };
